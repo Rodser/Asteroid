5f00abf8c5a5d10d20f44b0dc01a98e03fbe8f7b
Rodser <yarodser@ya.ru>
2021-07-15 12:41:04 +0400

    moveShip

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Projectile : MonoBehaviour
+    {
+         [SerializeField] private float speed = 5f;
+
+        private void Start()
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        private void Update()
+        {
+           transform.position += transform.up * speed * Time.deltaTime;           
+        }
+    }
+}
